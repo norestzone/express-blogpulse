f4856185d665198d5ba740031fef791b3e8aad69
ChassyAHustla <chassyahustla@ChassyAHustlas-MacBook-Pro.local>
2021-01-14 15:00:17 -0800

    part 1 complete

--- models/comment.js
+++ models/comment.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      models.comment.hasMany(models.article)
     }
   };
   comment.init({
