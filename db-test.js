46262711d4620bd4001cd098c262b993d732c5a1
ChassyAHustla <chassyahustla@ChassyAHustlas-MacBook-Pro.local>
2021-01-14 15:20:10 -0800

    part 1 updated

---
+++ db-test.js
@@ -0,0 +1,22 @@
+// const db = require('./models')
+
+// db.comment.create({
+//   name: 'Paul Allen',
+//   content: 'This is really neat! Thanks for posting.',
+//   articleId: 1
+// })
+// .then(comment => {
+//   console.log(comment.get())
+//   process.exit()
+// })
+
+const db = require('./models')
+
+db.article.findOne({
+  where: { id: 1 },
+  include: [db.comment]
+}).then(article => {
+  // by using eager loading, the article model should have a comments key
+  console.log(article.comments)
+  process.exit()
+})
--- models/comment.js
+++ models/comment.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      models.comment.hasMany(models.article)
+      models.comment.belongsTo(models.article)
     }
   };
   comment.init({
