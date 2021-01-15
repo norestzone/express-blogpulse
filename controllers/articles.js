81e4219e9b06a567726e9629b37a3bcf4fc79875
ChassyAHustla <chassyahustla@ChassyAHustlas-MacBook-Pro.local>
2021-01-14 17:39:20 -0800

    user comment functioning

--- controllers/articles.js
+++ controllers/articles.js
@@ -10,55 +10,54 @@ router.post('/', (req, res) => {
     content: req.body.content,
     authorId: req.body.authorId
   })
-  .then((post) => {
-    res.redirect('/')
-  })
-  .catch((error) => {
-    res.status(400).render('main/404')
-  })
+    .then((post) => {
+      res.redirect('/')
+    })
+    .catch((error) => {
+      res.status(400).render('main/404')
+    })
 })
 
-// router.post('/comment/:id', (req, res) => {
-//   db.article.findOne({
-//     where: { id: req.params.id },
-//     include: [db.author, db.comment]
-// })
-// .then(article => {
-//   article.createComment({
-//     name: req.body.name,
-//     content: req.body.content
-//   })
-//   .then(comment => {
-//     res.redirect(`/articles/${req.params.id}`)
-//   })
-// })
-
-// GET /articles/new - display form for creating new articles
-router.get('/new', (req, res) => {
-  db.author.findAll()
-  .then((authors) => {
-    res.render('articles/new', { authors: authors })
-  })
-  .catch((error) => {
-    res.status(400).render('main/404')
-  })
-})
-
-// GET /articles/:id - display a specific post and its author
-router.get('/:id', (req, res) => {
+router.post('/comment/:id', (req, res) => {
   db.article.findOne({
     where: { id: req.params.id },
     include: [db.author, db.comment]
-  })
-  .then((article) => {
-    if (!article) throw Error()
-    console.log(article.author)
-    res.render('articles/show', { article: article })
-  })
-  .catch((error) => {
-    console.log(error)
-    res.status(400).render('main/404')
+  }).then(article => {
+    article.createComment({
+      name: req.body.name,
+      content: req.body.content
+    }).then(comment => {
+      res.redirect(`/articles/${req.params.id}`)
+    })
   })
 })
 
-module.exports = router
+  // GET /articles/new - display form for creating new articles
+  router.get('/new', (req, res) => {
+    db.author.findAll()
+      .then((authors) => {
+        res.render('articles/new', { authors: authors })
+      })
+      .catch((error) => {
+        res.status(400).render('main/404')
+      })
+  })
+
+  // GET /articles/:id - display a specific post and its author
+  router.get('/:id', (req, res) => {
+    db.article.findOne({
+      where: { id: req.params.id },
+      include: [db.author, db.comment]
+    })
+      .then((article) => {
+        if (!article) throw Error()
+        console.log(article.author)
+        res.render('articles/show', { article: article })
+      })
+      .catch((error) => {
+        console.log(error)
+        res.status(400).render('main/404')
+      })
+  })
+
+  module.exports = router
